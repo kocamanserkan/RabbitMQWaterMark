2205fb5976e587c1f2483a05beb5ebc8210d5181
Serkan Kocaman <serkan.kocaman@logo.com.tr>
2024-09-13 13:32:14 +0300

    initial commit

---
+++ BackgroundServices/ImageWaterMarkProcessBackgroundService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Web.WaterMark.Services;
+
+namespace Web.WaterMark.BackgroundServices
+{
+    public class ImageWaterMarkProcessBackgroundService : BackgroundService
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+        private readonly ILogger<ImageWaterMarkProcessBackgroundService> _logger;
+        private IModel _channel;
+        public ImageWaterMarkProcessBackgroundService(RabbitMQClientService rabbitMQClientService, ILogger<ImageWaterMarkProcessBackgroundService> logger)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _channel = _rabbitMQClientService.Connect();
+            _channel.BasicQos(0, 1, false);
+
+            return base.StartAsync(cancellationToken);
+
+
+        }
+
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            _channel.BasicConsume(RabbitMQClientService.QueueName, false, consumer);
+
+
+            consumer.Received += ConcumerReceived;
+
+
+            return Task.CompletedTask;
+        }
+
+        private async Task ConcumerReceived(object sender, BasicDeliverEventArgs @event)
+        {
+            try
+            {
+                var imageCreatedEvent = JsonSerializer.Deserialize<productImageCreatedEvent>(Encoding.UTF8.GetString(@event.Body.ToArray()));
+                var siteName = "www.serkankocaman.com";
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imageCreatedEvent.ImageName);
+
+                using var img = Image.FromFile(path);
+
+                using var graphic = Graphics.FromImage(img);
+
+                var font = new Font(FontFamily.GenericMonospace, 32, FontStyle.Bold, GraphicsUnit.Pixel);
+                var textSize = graphic.MeasureString(siteName, font);
+                var color = Color.Black;
+                var brush = new SolidBrush(color);
+                var position = new Point(img.Width - ((int)textSize.Width + 30), img.Height - ((int)textSize.Height + 30));
+
+                graphic.DrawString(siteName, font, brush, position);
+
+                img.Save("wwwroot/images/watermarks/" + imageCreatedEvent.ImageName);
+
+                img.Dispose();
+                graphic.Dispose();
+
+                _channel.BasicAck(@event.DeliveryTag, false);
+
+            }
+            catch (Exception)
+            {
+                _logger.LogError("hata");
+            }
+
+        }
+    }
+}
---
+++ Services/productImageCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.WaterMark.Services
+{
+    public class productImageCreatedEvent
+    {
+        public string ImageName { get; set; }
+    }
+}
